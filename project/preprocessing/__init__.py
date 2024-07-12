d036f86bb92bd1b7978319404728dd3de83c2efa
Kess Entrata <kesselentrata@gmail.com>
2024-07-12 09:04:31 +0800

    setup file stucture and add imports and update gitignore

---
+++ project/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+from .transformations import get_transform
+from .data_augmentation import DataAugmentation
+from .interpolation import Interpolation
+from .normalization import Normalization
+from .skullstripping import SkullStripping
