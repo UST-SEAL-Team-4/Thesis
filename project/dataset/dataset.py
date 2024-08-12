8c5cb433b6b7f9cd79e36277ba0fcd6be3880906
Kess Entrata <kesselentrata@gmail.com>
2024-08-12 16:24:22 +0800

    add brief explanation in the dataset file

--- project/dataset/dataset.py
+++ project/dataset/dataset.py
@@ -12,11 +12,14 @@ class Dataset:
     def _load_cases(self):
         """
         Load all cases into a dictionary, grouping them by cohorts.
-        This method will check all relevant directories (raw MRI, CMB masks, and skull-stripped MRI) 
-        to identify cases.
+        It will check all directories (raw MRI, CMB masks, and skull-stripped MRI)
+        and add those in the dictionary.
         """
+        
         def add_case_to_cohort(case):
-            """ Helper function to add a case to the appropriate cohort """
+            """ 
+            Add the case number to their corresponding cohorts.
+            """
             if case.startswith('sub-1'):
                 if case not in self.cases['cohort1']:
                     self.cases['cohort1'].append(case)
@@ -27,30 +30,40 @@ class Dataset:
                 if case not in self.cases['cohort3']:
                     self.cases['cohort3'].append(case)
 
-        # Check raw MRI directory
-        if os.path.exists(self.raw_mri_dir):
-            for file in os.listdir(self.raw_mri_dir):
-                if file.startswith('sub-'):
-                    case = file.split('_')[0]
-                    add_case_to_cohort(case)
+        """ 
+        Check raw MRI directory 
+        For example, `sub-101_space-T2S_desc-masked_T2S.nii.gz` -> `sub-101`,
+        so it gets added to cohort1
+        """
+        for file in os.listdir(self.raw_mri_dir):
+            if file.startswith('sub-'):
+                case = file.split('_')[0]
+                add_case_to_cohort(case)
 
-        # Check CMB masks directory
-        if os.path.exists(self.cmb_masks_dir):
-            for file in os.listdir(self.cmb_masks_dir):
-                if file.startswith('sub-'):
-                    case = file.split('_')[0]
-                    add_case_to_cohort(case)
+        """ 
+        Check CMB masks directory 
+        For example, `sub-101_space-T2S_CMB.nii.gz` -> `sub-101`,
+        so it gets added to cohort1
+        """
+        for file in os.listdir(self.cmb_masks_dir):
+            if file.startswith('sub-'):
+                case = file.split('_')[0]
+                add_case_to_cohort(case)
 
-        # Check skull-stripped MRI directory
-        if os.path.exists(self.skullstripped_dir):
-            for file in os.listdir(self.skullstripped_dir):
-                if file.startswith('sub-'):
-                    case = file.split('_')[0]
-                    add_case_to_cohort(case)
+        """ 
+        Check skull-stripped MRI directory 
+        For example, `sub-101_space-T2S_desc-masked_T2S_stripped.nii.gz` -> `sub-101`,
+        so it gets added to cohort1
+        """
+        for file in os.listdir(self.skullstripped_dir):
+            if file.startswith('sub-'):
+                case = file.split('_')[0]
+                add_case_to_cohort(case)
     
     def load_cmb_masks(self, cohort_num=None):
         """
-        Load CMB mask files. Optionally, filter by cohort.
+        Load CMB mask files. There is also an option
+        to load them by a specific cohort.
         """
         masks = []
         cohorts = [f'cohort{cohort_num}'] if cohort_num else self.cases.keys()
@@ -58,14 +71,14 @@ class Dataset:
         for cohort in cohorts:
             for case in self.cases[cohort]:
                 mask_path = os.path.join(self.cmb_masks_dir, f"{case}_space-T2S_CMB.nii.gz")
-                if os.path.exists(mask_path):
-                    masks.append(mask_path)
+                masks.append(mask_path)
         
         return masks
     
     def load_raw_mri(self, cohort_num=None):
         """
-        Load raw MRI files. Optionally, filter by cohort.
+        Load raw MRI files. There is also an option
+        to load them by a specific cohort.
         """
         mri_files = []
         cohorts = [f'cohort{cohort_num}'] if cohort_num else self.cases.keys()
@@ -73,14 +86,14 @@ class Dataset:
         for cohort in cohorts:
             for case in self.cases[cohort]:
                 mri_path = os.path.join(self.raw_mri_dir, f"{case}_space-T2S_desc-masked_T2S.nii.gz")
-                if os.path.exists(mri_path):
-                    mri_files.append(mri_path)
+                mri_files.append(mri_path)
         
         return mri_files
     
     def load_skullstripped_mri(self, cohort_num=None):
         """
-        Load skull-stripped MRI files. Optionally, filter by cohort.
+        Load skull-stripped MRI files. There is also an option
+        to load them by a specific cohort.
         """
         stripped_files = []
         cohorts = [f'cohort{cohort_num}'] if cohort_num else self.cases.keys()
@@ -88,8 +101,7 @@ class Dataset:
         for cohort in cohorts:
             for case in self.cases[cohort]:
                 stripped_path = os.path.join(self.skullstripped_dir, f"{case}_space-T2S_desc-masked_T2S_stripped.nii.gz")
-                if os.path.exists(stripped_path):
-                    stripped_files.append(stripped_path)
+                stripped_files.append(stripped_path)
         
         return stripped_files
     
@@ -98,68 +110,3 @@ class Dataset:
         Get the number of cases in each cohort.
         """
         return {cohort: len(cases) for cohort, cases in self.cases.items()}
-
-# class Dataset:
-#     FOLDER_DIR = 'VALDO_Dataset\Task2'
-#     PREPROCESSED_DIR = 'VALDO_Dataset\preprocessed_dataset'
-#     LABEL_FNAME = '_space-T2S_CMB.nii.gz'
-#     ID_FNAME = '_space-T2S_desc-masked_T2S.nii.gz'
-#     ID_PREPROCESSED_FNAME = '_space-T2S_desc-masked_T2S_stripped.nii.gz'
-    
-#     def __init__(self):
-#         self.current_dir = os.path.abspath(os.path.join(os.getcwd(), '../', self.FOLDER_DIR))
-#         self.preprocessed_dir = os.path.abspath(os.path.join(os.getcwd(), '../', self.PREPROCESSED_DIR))
-#         self.cases = {'cohort1': [], 'cohort2': [], 'cohort3': []}
-#         self._load_cases()
-        
-#     def _load_cases(self):
-#         folders = [item for item in os.listdir(self.current_dir) if os.path.isdir(os.path.join(self.current_dir, item))]
-
-#         for folder in folders:
-#             if 'sub-1' in folder:
-#                 self.cases['cohort1'].append(folder)
-#             elif 'sub-2' in folder:
-#                 self.cases['cohort2'].append(folder)
-#             else:
-#                 self.cases['cohort3'].append(folder)
-                
-#     def get_cohort_lengths(self):
-#          return {cohort: len(cases) for cohort, cases in self.cases.items()}
-
-#     def generate_labels_and_ids(self, cohort_num=None):
-#         labels = []
-#         ids = []
-        
-#         if cohort_num:
-#             cohorts = [f'cohort{cohort_num}']
-#         else:
-#             cohorts = self.cases.keys()
-            
-#         for cohort in cohorts:
-#             for case in self.cases[cohort]:
-#                 label = f'{self.current_dir}\\{case}\\{case}{self.LABEL_FNAME}'
-#                 id = f'{self.current_dir}\\{case}\\{case}{self.ID_FNAME}'
-#                 labels.append(label)
-#                 ids.append(id)
-            
-#         return labels, ids
-
-#     def get_all_labels_and_ids(self):
-#         return self.generate_labels_and_ids()
-    
-#     def get_labels_and_ids_by_cohort(self, cohort_num):
-#         return self.generate_labels_and_ids(cohort_num)
-
-#     def get_preprocessed_labels_and_ids(self):
-#         labels = []
-#         ids = []
-        
-#         for cohort in self.cases.keys():
-#             for case in self.cases[cohort]:
-#                 label = f'{self.current_dir}\\{case}\\{case}{self.LABEL_FNAME}'
-#                 id = f'{self.current_dir}\\{case}{self.ID_PREPROCESSED_FNAME}'
-                
-#                 if os.path.exists(label) and os.path.exists(id):
-#                     labels.append(label)
-#                     ids.append(id)
-#         return labels, ids
