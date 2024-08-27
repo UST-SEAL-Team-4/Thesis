e49ef184bde7d17e5bce48196aaf0b4ca4f8ece0
1123Alessandro <alessandroandyrei@gmail.com>
2024-08-28 02:52:49 +0800

    initial RPN model; imported rpn in init of projects folder

---
+++ project/model/rpn.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class RPN(nn.Module):
+    def __init__(self, input_dim, output_dim, nh, dim_ff):
+        super().__init__()
+
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=input_dim, nhead=nh, dim_feedforward=dim_ff)
+        self.trans_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=1)
+        self.fc = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+
+        x = self.trans_encoder(x)
+        x = self.fc(x)
+
+        return x
