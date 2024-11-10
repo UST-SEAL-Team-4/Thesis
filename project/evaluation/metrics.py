6ee2ef104546287cb3305f8bc1c0681ed3a5d370
LexLorenzo <lexzedrick.lorenzo.cics@ust.edu.ph>
2024-11-10 23:32:09 +0800

    Applying metrics to vit, rpn training, rpn metric, and vit metric

--- project/evaluation/metrics.py
+++ project/evaluation/metrics.py
@@ -1,79 +1,92 @@
-import pandas as pd
-from project.utils.euclid_dist import get_euclid_dist
+from torch import Tensor
+import torch
+from torchmetrics.functional.detection import intersection_over_union
+from torchvision.ops import box_area
+from typing import Tuple
+import numpy as np
 
-def count_metrics(all_marking, predicted_marking):
-    fp = pd.DataFrame(columns=['image_id', 'slice_num', 'x', 'y', 'w', 'h'])
-    fp_count = 0
-    tp = pd.DataFrame(columns=['image_id', 'slice_num', 'x', 'y', 'w', 'h'])
-    tp_count = 0
-    fn = pd.DataFrame(columns=['image_id', 'slice_num', 'x', 'y', 'w', 'h'])
-    fn_count = 0
+def _upcast(t: Tensor) -> Tensor:
+    # Protects from numerical overflows in multiplications by upcasting to the equivalent higher type
+    if t.is_floating_point():
+        return t if t.dtype in (torch.float32, torch.float64) else t.float()
+    else:
+        return t if t.dtype in (torch.int32, torch.int64) else t.int()
 
-    merged_df = pd.merge(predicted_marking, all_marking, on=[
-                         'image_id', 'slice_num'], suffixes=('_pred', '_true'))
+def _box_inter_union(boxes1: Tensor, boxes2: Tensor) -> Tuple[Tensor, Tensor]:
+    area1 = box_area(boxes1)
+    area2 = box_area(boxes2)
 
-    predicted_marking['key'] = predicted_marking['image_id'] + \
-        '_' + predicted_marking['slice_num'].astype(str)
-    merged_df['key'] = merged_df['image_id'] + \
-        '_' + merged_df['slice_num'].astype(str)
+    lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
+    rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
 
-    fp = predicted_marking[~predicted_marking['key'].isin(merged_df['key'])]
-    fp = fp.drop(columns=['key'])
-    fp_count += len(fp)
+    wh = _upcast(rb - lt).clamp(min=0)  # [N,M,2]
+    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
 
-    grouped_dict = {}
+    union = area1[:, None] + area2 - inter
 
-    grouped = merged_df.groupby(['image_id', 'slice_num'])
+    return inter, union
 
-    for (image_id, slice_num), group in grouped:
-        key = (image_id, slice_num)
-        grouped_dict[key] = group
+def dice_coef(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    total_sum = np.sum(pred_mask) + np.sum(groundtruth_mask)
+    if total_sum == 0:
+        return 0
+    dice = np.mean(2*intersect/total_sum)
+    return round(dice, 4) #round up to 4 decimal places
 
-    for key, df in grouped_dict.items():
-        x_pred_values = df['x_pred'].values
-        y_pred_values = df['y_pred'].values
-        x_true_values = df['x_true'].values
-        y_true_values = df['y_true'].values
+def precision_score_(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    total_pixel_pred = np.sum(pred_mask)
+    if total_pixel_pred == 0:
+        return 0
+    precision = np.mean(intersect/total_pixel_pred)
+    return round(precision, 4)
 
-        w_pred_values = df['w_pred'].values
-        h_pred_values = df['h_pred'].values
+def recall_score_(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    total_pixel_truth = np.sum(groundtruth_mask)
+    if total_pixel_truth == 0:
+        return 0
+    recall = np.mean(intersect/total_pixel_truth)
+    return round(recall, 4)
 
-        is_correct = False
-        for i in range(len(x_pred_values)):
-            pred_cmb = [x_pred_values[i], y_pred_values[i]]
-            true_cmb = [x_true_values[i], y_true_values[i]]
-            dist = get_euclid_dist(pred_cmb, true_cmb)
-            if dist > 20:
-                is_correct = False
-            else:
-                is_correct = True
-                break
+def accuracy(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    union = np.sum(pred_mask) + np.sum(groundtruth_mask) - intersect
+    xor = np.sum(groundtruth_mask==pred_mask)
+    if (union + xor - intersect) == 0:
+        return 0
+    acc = np.mean(xor/(union + xor - intersect))
+    return round(acc, 4)
 
-        new_row = {
-            'image_id': key[0],
-            'slice_num': key[1],
-            'x': x_pred_values[i],
-            'y': y_pred_values[i],
-            'w': w_pred_values[i],
-            'h': h_pred_values[i]
-        }
-        temp = pd.DataFrame(new_row, index=[0])
+def iou(groundtruth_mask, pred_mask):
+    intersect = np.sum(pred_mask*groundtruth_mask)
+    union = np.sum(pred_mask) + np.sum(groundtruth_mask) - intersect
+    if union == 0:
+        return 0
+    iou = np.mean(intersect/union)
+    return round(iou, 4)
 
-        if is_correct:
-            tp_count += 1
-            tp = pd.concat([tp, temp], ignore_index=True)
-        else:
-            fp_count += 1
-            fp = pd.concat([fp, temp], ignore_index=True)
+def isa_rpn_metric(image_size, target_bbox, predicted_bbox):
+    iou_score = intersection_over_union(predicted_bbox * image_size, target_bbox * image_size)
 
-    all_marking['key'] = all_marking['image_id'] + \
-        '_' + all_marking['slice_num'].astype(str)
-    tp['key'] = tp['image_id'] + '_' + tp['slice_num'].astype(str)
-    
-    fn = all_marking[all_marking['key'].isin(tp['key'])]
-    fn = fn.drop(columns=['key'])
-    fn_count += len(fn)
+    inter, union = _box_inter_union(predicted_bbox * image_size, target_bbox * image_size)
 
-    tp = tp.drop(columns=['key'])
+    precision_score = (inter / box_area(predicted_bbox * image_size))
+    recall_score = (inter / box_area(target_bbox * image_size))
 
-    return fp, fp_count, tp, tp_count, fn, fn_count
+    if any([precision_score, recall_score]) == 0:
+        f1_score = 0
+    else:
+        f1_score = (2 * (precision_score * recall_score)) / (precision_score + recall_score)
+        f1_score = f1_score.detach().cpu().numpy()
+
+    return iou_score, precision_score, recall_score, f1_score
+
+def isa_vit_metric(predicted_segmentation, true_segmentation):
+    dice_score = dice_coef(true_segmentation, predicted_segmentation)
+    precision_score = precision_score_(true_segmentation, predicted_segmentation)
+    recall_score = recall_score_(true_segmentation, predicted_segmentation)
+    accuracy_score = accuracy(true_segmentation, predicted_segmentation)
+
+    return dice_score, precision_score, recall_score, accuracy_score
