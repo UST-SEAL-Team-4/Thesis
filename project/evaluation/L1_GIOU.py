829d850a2ec68300a86eb01a6949b603dcc7a7c2
nigelhaim <nigelsebastian21@gmail.com>
2024-11-22 00:44:43 +0800

    Added sigmoid function

--- project/evaluation/L1_GIOU.py
+++ project/evaluation/L1_GIOU.py
@@ -7,7 +7,7 @@ class L1_GIOU():
     def loss(pred, target, alpha = 1, beta = 1):
         l1 = nn.L1Loss()
         l1_loss = alpha * l1(pred, target)
-        giou_loss = beta * tvops.generalized_box_iou_loss(pred, target, reduction='mean')
+        giou_loss = beta * (tvops.generalized_box_iou_loss(pred, target, reduction='mean').sigmoid())
         return l1_loss + giou_loss
        
     def __call__(self):
