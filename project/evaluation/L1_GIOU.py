58c5c0457ad91d99ad95ce199c516c065df8c904
nigelhaim <nigelsebastian21@gmail.com>
2024-11-21 20:48:52 +0800

    Changed code structure for simplicity

--- project/evaluation/L1_GIOU.py
+++ project/evaluation/L1_GIOU.py
@@ -2,9 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision.ops as tvops
 
+
 class L1_GIOU():
     def loss(pred, target, alpha = 1, beta = 1):
         l1 = nn.L1Loss()
-        return alpha * l1(pred, target) + beta * tvops.generalized_box_iou_loss(pred, target, reduction='mean')
+        l1_loss = alpha * l1(pred, target)
+        giou_loss = beta * tvops.generalized_box_iou_loss(pred, target, reduction='mean')
+        return l1_loss + giou_loss
+       
     def __call__(self):
         return f'L1_GIOU: '
