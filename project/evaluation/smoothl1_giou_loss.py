1d95c098908f4a3c13d9c934a66015d354bf451a
1123Alessandro <alessandroandyrei@gmail.com>
2024-12-08 11:11:58 +0800

    fix: make BoxesLoss work with batches

--- project/evaluation/smoothl1_giou_loss.py
+++ project/evaluation/smoothl1_giou_loss.py
@@ -14,11 +14,17 @@ class SmoothL1GiouLoss():
         return f'SmoothL1GiouLoss: '
 
 class BoxesLoss():
-    def loss(pred, target, alpha=0.5, beta=0.5):
+    def loss(pred, target, alpha=1, beta=1):
         l1 = nn.SmoothL1Loss()
         bce = nn.BCEWithLogitsLoss()
 
-        l1L = alpha * l1(pred[:, 1:], target[:, 1:])
-        bceL = beta * bce(pred[:, :1], target[:, :1])
+        assert pred.ndim == 3, 'Predictions must have ndim of 3, SAMPLE X REGION X OUTPUTS'
+        assert target.ndim == 3, 'Targets must have ndim of 3, SAMPLE X REGION X OUTPUTS'
+        assert pred.shape[0] == target.shape[0], 'The number of outputs and ground truth must be the same'
+        assert pred.shape[1] == target.shape[1], 'The predicted regions and the ground truth regions must be equal'
+        assert len(target[:, :, 0].unique()) == 2, 'Ground truth classification feature should only be either 0 or 1'
+
+        l1L = alpha * l1(pred[:, :, 1:], target[:, :, 1:])
+        bceL = beta * bce(pred[:, :, :1], target[:, :, :1])
 
         return l1L + bceL
